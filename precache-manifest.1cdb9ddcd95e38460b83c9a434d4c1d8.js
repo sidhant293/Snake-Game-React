b42ac3dac06a7a31440f1c86895fdb3a16a3556a
sidhant <sidhantsharma293@gmail.com>
2020-06-13 17:36:56 +0530

    Updates

---
+++ precache-manifest.1cdb9ddcd95e38460b83c9a434d4c1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f1a0d2f5926686b0f1d3cdf8dea40a",
+    "url": "/Snake-Game-React/index.html"
+  },
+  {
+    "revision": "549649dda698b6bc25ff",
+    "url": "/Snake-Game-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2d0c1df98d4201982f1a",
+    "url": "/Snake-Game-React/static/css/main.e83db697.chunk.css"
+  },
+  {
+    "revision": "549649dda698b6bc25ff",
+    "url": "/Snake-Game-React/static/js/2.dcd20506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake-Game-React/static/js/2.dcd20506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0c1df98d4201982f1a",
+    "url": "/Snake-Game-React/static/js/main.ea655268.chunk.js"
+  },
+  {
+    "revision": "691aff9f5fc7ce24e5a5",
+    "url": "/Snake-Game-React/static/js/runtime-main.23f18248.js"
+  }
+]);
